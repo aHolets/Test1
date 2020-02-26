7ec1a454c8adafdcd3e80a550a52a23658dc138e
aholets <aholets@edgegravity.ericsson.com>
2020-02-26 16:58:07 +0200

    init13

--- cicd/component.groovy
+++ cicd/component.groovy
@@ -1,2 +1,31 @@
-echo "PLUGIN ::: unitTests "
+
+def unitTests(ctx) {
+    echo "PLUGIN ::: unitTests "
+
+/* withDockerContainer(image:'maven:3.6.3-jdk-8-slim'){
+        sh 'mvn test'
+    }*/
+
+}
+
+def build(ctx) {
+    echo "PLUGIN ::: build "
+}
+
+def componentTests(ctx) {
+    echo "PLUGIN ::: componentTests "
+}
+
+return [
+    name: 'plugin',
+    type: 'library',
+    language: 'groovy',
+    pkg: [
+        type: 'repo/tag'
+    ],
+    build: this.&build,
+    test: [
+        unit: this.&unitTests
+    ]
+]
 
